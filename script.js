0e7293ad9de1d2a9ef813a381ab733c8d77404c0
Artem <artemrudenko971@gmail.com>
2020-03-26 15:10:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// alert('Hello');
+// Возвращает true/false на ответ
+// let answer = confirm ('Are u here?');
+// console.log(answer);
+
+// let answer = + prompt("Есть ли вам 18?", "да"); / Плюс перед prompt превращает строку в число
+// console.log(typeof(answer));
+
+// console.log ('arr' + " - objcet");
+// console.log (4 + " - objcet");
+
+// 
+let incr = 10,
+	decr = 10;
+
+console.log(incr++);
+console.log(decr--);
